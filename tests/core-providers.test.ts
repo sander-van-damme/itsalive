e3a7c7cabbe4ae0a304dc252b8c02c87807fc3b9
Sander <73292023+sander-van-damme@users.noreply.github.com>
2026-09-19 13:48:35 +0200

    Remove stale provider test import

--- tests/core-providers.test.ts
+++ tests/core-providers.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, describe, expect, it, vi } from "vitest";
-import { createDefaultRegistry, createHttpAdapter, ProviderRegistry, ProviderResponseError } from "../src/shell/core/providers";
+import { createHttpAdapter, ProviderRegistry, ProviderResponseError } from "../src/shell/core/providers";
 
 describe("ProviderRegistry", () => {
   afterEach(() => {
